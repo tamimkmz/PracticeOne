306fd4715999459b70678e30898bd2ff84fe6294
tamimkmz <tamim.kmz@gmail.com>
2023-08-02 20:03:41 -0700

    changes

--- src/gitpractice03.java
+++ src/gitpractice03.java
@@ -2,7 +2,8 @@ public class gitpractice03 {
     public static void main (String[] args) {
         System.out.println("this is a new code");
         System.out.println("Hello");
-
+        System.out.println(" I love you");
+        System.out.println("I love you too");
     }
 
 }
