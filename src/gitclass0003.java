87b974fb97777c6f0a4a90a61dd6c92bbacbebb0
tamimkmz <tamim.kmz@gmail.com>
2023-08-02 18:07:25 -0700

    second commit

--- src/gitclass0003.java
+++ src/gitclass0003.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class gitclass0003 {
+public class gitclass0003 {
+    public static void main (String[] args) {
+        boolean productAvailable=true;
+
+
+    }
 }
--- src/gitpractice03.java
+++ src/gitpractice03.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class gitpractice03 {
+public class gitpractice03 {
+    public static void main (String[] args) {
+        System.out.println("this is a new code");
+        System.out.println("Hello");
+
+    }
+
 }
